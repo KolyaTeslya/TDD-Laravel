7ee0f32ac6eb088ec2e4c85962fafd92a0dee284
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-07-09 17:58:24 +0300

    add new tests

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/posts', [\App\Http\Controllers\PostController::class, 'store']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
+Route::post('/posts', [\App\Http\Controllers\PostController::class, 'store']);
+Route::get('/posts/{post}', [\App\Http\Controllers\PostController::class, 'show']);
+Route::patch('/posts/{post}', [\App\Http\Controllers\PostController::class, 'update']);
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Post;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Testing\File;
 use Illuminate\Support\Facades\Storage;
@@ -13,12 +14,16 @@ class PostTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Storage::fake('local');
+    }
+
     /** @test */
     public function a_post_can_be_stored()
     {
         $this->withoutExceptionHandling();
-
-        Storage::fake('local');
 
         $file = File::create('my_image.jpeg');
 
@@ -38,11 +43,101 @@ class PostTest extends TestCase
 
         $this->assertEquals($data['title'], $post->title);
         $this->assertEquals($data['description'], $post->description);
-
         $this->assertEquals('images/' . $file->hashName(), $post->image_url);
 
         Storage::disk('local')->assertExists($post->image_url);
 
     }
+
+    /** @test */
+    public function attribute_title_is_required_for_storing_post()
+    {
+        $data = [
+            'title' => '',
+            'description' => 'Description',
+            'image' => ''
+        ];
+        $res = $this->post('/posts', $data);
+
+        $res->assertRedirect();
+        $res->assertInvalid('title');
+    }
+
+    /** @test */
+    public function attribute_image_is_file_for_storing_post()
+    {
+        $data = [
+            'title' => 'Title',
+            'description' => 'Description',
+            'image' => 'dwded'
+        ];
+        $res = $this->post('/posts', $data);
+
+        $res->assertRedirect();
+        $res->assertInvalid('image');
+    }
+
+    /** @test */
+
+    public function a_post_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $post = Post::factory()->create();
+        $file = File::create('image.jpeg');
+
+        $data = [
+            'title' => 'Title edited',
+            'description' => 'Description edited',
+            'image' => $file
+        ];
+
+        $res = $this->patch('/posts/' . $post->id, $data);
+
+        $res->assertOk();
+
+        $updatedPost = Post::first();
+        $this->assertEquals($data['title'], $updatedPost->title);
+        $this->assertEquals($data['description'], $updatedPost->description);
+        $this->assertEquals('images/' . $file->hashName(), $updatedPost->image_url);
+
+        $this->assertEquals($post->id, $updatedPost->id);
+    }
+
+    /** @test */
+    public function response_for_route_posts_index_is_view_post_index_with_posts()
+    {
+        $this->withoutExceptionHandling();
+
+        $posts = Post::factory(10)->create();
+
+        $res = $this->get('/posts');
+
+        $res->assertViewIs('posts.index');
+
+        $res->assertSeeText('View page');
+
+        $titles = $posts->pluck('title')->toArray();
+        $res->assertSeeText($titles);
+    }
+
+    /** @test */
+    public function response_for_route_posts_show_is_view_post_show_with_single_post()
+    {
+        $this->withoutExceptionHandling();
+        $post = Post::factory()->create();
+
+        $res = $this->get('/posts/' . $post->id);
+
+        $res->assertViewIs('posts.show');
+        $res->assertSeeText('Show page');
+        $res->assertSeeText($post->title);
+        $res->assertSeeText($post->description);
+
+    }
+
+
+
+
 
 }
